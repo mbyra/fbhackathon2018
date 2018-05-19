d42503e92c92a8345e91ecc8c94ce26f3a8b217e
marcin <marbit12@gmail.com>
2018-05-19 17:11:42 +0200

    mozna robic szablon

--- LiveLecture/models.py
+++ LiveLecture/models.py
@@ -1,3 +1,32 @@
 from django.db import models
+from django.core.exceptions import ValidationError
+from django.db.models import Q
+from datetime import timedelta
+from django.contrib.auth.models import AbstractUser
 
-# Create your models here.
+
+class User(AbstractUser):
+    first_name = models.CharField(max_length=20)
+    last_name = models.CharField(max_length=20)
+    is_lecturer = models.NullBooleanField()
+
+    def __str__(self):
+        return 'User %s %s, lecturer: %s' % (self.first_name, self.last_name, self.is_lecturer)
+
+class Lecture(models.Model):
+    lecturer = models.ForeignKey(User, on_delete=models.CASCADE)
+    name = models.CharField(max_length=50, unique=True)
+    description = models.CharField(max_length=500)
+    max_students = models.IntegerField()
+
+    def __str__(self):
+        return 'Lecture: %s' % self.name
+
+class Subscription(models.Model):
+    student = models.ForeignKey(User, on_delete=models.CASCADE)
+    lecture = models.ForeignKey(Lecture, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return 'Substription of %s to lecture: %s' % (self.student, self.lecture)
+
+
--- LiveLecture/views.py
+++ LiveLecture/views.py
@@ -1,5 +1,12 @@
-from django.http import HttpResponse
-
+from .models import User
+from django.contrib.auth import authenticate, login, logout
+from django.db import transaction
+from django.shortcuts import render, get_object_or_404
+from django.http import HttpResponse, HttpResponseRedirect
+from django.template import loader
+from django.urls import reverse
+from datetime import datetime, timedelta
 
 def index(request):
-    return HttpResponse("Hello, world. You're at the LiveLecture index.")
+    context = {}
+    return render(request, 'LiveLecture/base.html', context)
