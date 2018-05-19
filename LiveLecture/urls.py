aaa776ac1d02f45eb0ccfd983931075669823ece
marcin <marbit12@gmail.com>
2018-05-19 21:44:58 +0200

    you can log in and og out

--- LiveLecture/urls.py
+++ LiveLecture/urls.py
@@ -2,7 +2,10 @@ from django.urls import path
 
 from . import views
 
+app_name = 'LiveLecture'
 urlpatterns = [
     path('', views.index, name='index'),
     path('logout/', views.logout_view, name='logout_view'),
+    path('login/', views.login_view, name='login_view'),
+    path('login_or_register/', views.login_or_register, name='login_or_register'),
 ]
--- LiveLecture/views.py
+++ LiveLecture/views.py
@@ -1,3 +1,5 @@
+from django.views.decorators.http import require_POST
+
 from .models import User, Lecture
 from django.contrib.auth import authenticate, login, logout
 from django.db import transaction
@@ -17,3 +19,23 @@ def logout_view(request):
     logout(request)
     context = {}
     return render(request, 'LiveLecture/upcoming.html', context)
+
+def login_view(request):
+    context = {}
+    return render(request, 'LiveLecture/login.html', context)
+
+@transaction.atomic
+@require_POST
+def login_or_register(request):
+    user = authenticate(username=request.POST['username'], password=request.POST['password'])
+    if user is not None:
+        print("uzytkownik istnieje")
+        login(request, user)
+        login_success = True
+        context = {'login_success': login_success}
+        return render(request, 'LiveLecture/upcoming.html', context)
+    else:
+        print("uzytkownik nie istnieje")
+        login_failure = True
+        context = {'login_failure': login_failure}
+        return render(request, 'LiveLecture/upcoming.html', context)
