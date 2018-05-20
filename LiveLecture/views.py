5bd42e34034b1b655ce853ee7495f88dec21e11a
marcin <marbit12@gmail.com>
2018-05-20 02:17:11 +0200

    messages are there!

--- LiveLecture/views.py
+++ LiveLecture/views.py
@@ -1,3 +1,4 @@
+from django.core.exceptions import ValidationError
 from django.views.decorators.http import require_POST
 
 from .models import User, Lecture, Subscription, Messages
@@ -52,4 +53,29 @@ def lecture(request, pk):
         students.append(substription.student)
 
     context = {'lecture' : lecture, 'students' : students}
+    return render(request, 'LiveLecture/lecture.html', context)
+
+
+def add_message(request, pk):
+    if 'message' not in request.POST:
+        raise ValidationError("No message in POST")
+
+    print(request.POST['message'])
+
+    template = loader.get_template('LiveLecture/lecture.html')
+    lecture = Lecture.objects.filter(pk=pk)[0]
+    if lecture is None:
+        raise ValueError("There is no lecture with this pk!")
+
+    substriptions = Subscription.objects.filter(lecture=lecture)
+    students = []
+    for substription in substriptions:
+        students.append(substription.student)
+
+    Messages.objects.create(lecture=lecture, student=request.user, content=request.POST['message'], likes=0)
+
+    messages = Messages.objects.filter(lecture=lecture)
+
+    context = {'lecture': lecture, 'students': students, 'messages' : messages}
+
     return render(request, 'LiveLecture/lecture.html', context)
